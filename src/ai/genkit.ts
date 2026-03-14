9767b6395f8403e5e443faf2b8149b375376dc82
Bruno Antunes <brunoantunes100@gmail.com>
2026-03-14 14:15:32 +0000

    Gemini pro not fund

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,14 @@
+'use server';
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
-    googleAI(),
+    googleAI({
+      location: 'us-central1',
+    }),
   ],
 });
 
-export const geminiModel = 'gemini-pro';
+export const geminiModel = 'gemini-1.0-pro-001';
