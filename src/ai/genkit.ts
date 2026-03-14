4de54532b0dc59e1618635fcf8198f9601cdcd23
Bruno Antunes <brunoantunes100@gmail.com>
2026-03-14 14:06:06 +0000

    Gemini 1.0 pro not fund acho que vc precisa ver qual a configuração corr

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,10 +3,8 @@ import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({
-      location: 'us-central1',
-    }),
+    googleAI(),
   ],
 });
 
-export const geminiModel = 'gemini-1.0-pro';
+export const geminiModel = 'gemini-pro';
